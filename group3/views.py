55914acf614b4ec9ed54bfbaab95d0fd37c6b4f1
Tatchagon Koonkoei <o_k_t@hotmail.com>
2015-04-15 00:06:43 +0700

    add view data for Prof2Lang

--- group3/views.py
+++ group3/views.py
@@ -4,10 +4,10 @@ from group3.models import *
 # Create your views here.
 def prof2lang_index(request):
     template = 'group3/prof2lang_index.html'    # get template
-    prof2langList = Prof2Lang.objects.all()     # get all Prof2Lang objects
+    teachList = Teach.objects.all()     # get all Prof2Lang objects
 
     return render(
         request,
         template,
-        {'prof2langList': prof2langList}
+        {'teachList': teachList}
     )
