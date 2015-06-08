6f835f57fb749a86918341d4a11beb2012084a33
Tatchagon Koonkoei <o_k_t@hotmail.com>
2015-06-08 11:47:04 +0700

    make view of each teach in Prof2Lang

--- group3/urls.py
+++ group3/urls.py
@@ -3,4 +3,5 @@ from group3 import views
 
 urlpatterns = patterns('',
     url(r'^prof2lang/$', views.prof2lang_index, name='prof2lang_index'),
+    url(r'^prof2lang/(\d+)/$', views.prof2lang_view, name='prof2lang_view')
 )
