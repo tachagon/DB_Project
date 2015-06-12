d167d58dbf1c22b29d590153cef97066813c0f28
Phongyanon <phongyanon@gmail.com>
2015-06-12 15:30:12 +0700

    get data from DB to pdf

--- group3/urls.py
+++ group3/urls.py
@@ -4,6 +4,6 @@ from group3 import views
 urlpatterns = patterns('',
     url(r'^prof2lang/$', views.prof2lang_index, name='prof2lang_index'),
     url(r'^prof2lang/(\d+)/$', views.prof2lang_view, name='prof2lang_view'),
-    url(r'^testpdf/$', views.genpdf, name='genpdf')
+    url(r'^testpdf/(\d+)/$', views.genpdf, name='genpdf')
 
 )
--- group3/views.py
+++ group3/views.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
 from group3.models import *
 from django.http import HttpResponse
-
+from fpdf import FPDF
 # Create your views here.
 def prof2lang_index(request):
     template = 'group3/prof2lang_index.html'    # get template
@@ -27,13 +29,44 @@ def prof2lang_view(request, profID):
         context
     )
 
-def genpdf(request):
-    with open('group3/uni.pdf', 'rb') as pdf:
+def genpdf(request, profID):
+    teachObj = Teach.objects.get(pk= int(profID)) 
+    pdf = FPDF('P', 'mm', 'A4')
+    pdf.add_page()
+    
+    pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
+    pdf.set_font('DejaVu', '', 14)
+    
+    text = u"hello thai language................."
+    pdf.write(10, text)
+    pdf.image('group3/logo.png',10,20,33)
+    pdf.ln(8)
+    
+    pdf.add_font('Waree', '', 'Waree.ttf', uni=True)
+    pdf.set_font('Waree', '', 14)
+    pdf.cell(0, 10, u'' + teachObj.prof.profID)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.firstName + '   '+ teachObj.prof.lastName)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.shortName)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.department)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.faculty)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.sahakornAccount)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.tell)
+    pdf.ln(8)
+    pdf.cell(0, 10, u'' +teachObj.prof.email)
+                                
+    pdf.ln(20)
+    pdf.output("group3/uni.pdf", 'F')
+    
+    with open('group3/uni.pdf', 'rb') as pdf: # path to pdf in directory views.
         response = HttpResponse(pdf.read(),content_type='application/pdf')
         response['Content-Disposition'] = 'filename=uni.pdf'
         return response
     pdf.closed
     
-    #template = 'group3/hello.html'
-    #return render(request, template, {})
 
