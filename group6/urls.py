9fe2072f5e0f86dda4db88ae4648dba3d2044e23
Wittawin Kahuttanaseth <wittawin_k@hotmail.com>
2015-06-23 01:15:26 +0700

    Create 3 form

--- group6/urls.py
+++ group6/urls.py
@@ -4,6 +4,7 @@ from group6 import views
 urlpatterns = patterns('',
     url(r'^Project_Docs/$', views.index, name='project_docs_index'),
     url(r'^Project_Docs/create_3forms/$', views.create_3forms, name='project_docs_create_3forms'),
+    url(r'^Project_Docs/create_3forms/add$', views.create_3forms_add, name='project_docs_create_3forms_add'),
     url(r'^Project_Docs/view_approveProject/(\d+)/$', views.approveProject, name='project_docs_approveProject'),
     url(r'^Project_Docs/view_offerProject/(\d+)/$', views.offerProject, name='project_docs_offerProject'),
     url(r'^Project_Docs/view_researchProject/(\d+)/$', views.researchProject, name='project_docs_researchProject'),
