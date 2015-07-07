a829f7476aad8a4dcd620744472f224ad8df088c
Tatchagon Koonkoei <o_k_t@hotmail.com>
2015-07-07 23:49:38 +0700

    merge group7 and fix bug

--- static/js/application.js
+++ static/js/application.js
@@ -3,7 +3,7 @@ $(document).scroll(function(e){
     var hei = $(document).height();
     var headerHei = $('header').height();
     
-    if(scrollTop > (headerHei) && hei > 1200){
+    if(scrollTop > (headerHei) && hei > 2000){
         console.log(scrollTop);
         $('#navbar-menu').removeClass('navbar-static-top').addClass('navbar-fixed-top');
     } else {
